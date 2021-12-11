236ecbbcf414150f6738357350513bd4b734ec98
SReject <SReject@users.noreply.github.com>
2021-12-10 22:44:44 -0500

    Update test suite

--- test/evaluate.mjs
+++ test/evaluate.mjs
@@ -4,124 +4,173 @@ import * as errors from '../src/errors.mjs';
 
 import evaluate from '../src/evaluate.mjs';
 
+const expectThrow = async (fnc, type) => {
+    try {
+        await fnc();
+    } catch (err) {
+        if (type && !(err instanceof type)) {
+            throw new Error('threw incorrect error');
+        }
+        return;
+    }
+    throw new Error('did not throw an error');
+};
+
+const expectEqual = async (fnc, value) => {
+    let result = await fnc();
+    assert.equal(result, value);
+};
+
+const vars = [
+    {handle: 'txt', argsCheck: () => {}, evaluator: () => 'evaled_var_text'},
+    {handle: 'ten', argsCheck: () => {}, evaluator: () => 10},
+    {handle: 'sum', argsCheck: () => {}, evaluator: (meta, ...args) => {
+        return args.map(item => Number(item)).reduce((acc, cur) => acc + cur, 0)
+    }}
+];
+
 describe('evaluate()', function () {
+
     describe('Throws an error when arguments\'', function () {
-        it('variable-handlers is undefined', function () {
-            assert.throws(() => { evaluate(); }, TypeError);
+        it('variable-handlers is undefined', async function () {
+            await expectThrow(() => evaluate(), TypeError);
         });
-        it('variable-handlers is not an Array', function () {
-            assert.throws(() => { evaluate(''); }, TypeError);
+        it('variable-handlers is not an Array', async function () {
+            await expectThrow(() => evaluate(''), TypeError);
         });
-        it('options is undefined', function () {
-            assert.throws(() => { evaluate([]); }, TypeError)
+        it('options is undefined', async function () {
+            await expectThrow(() => evaluate(vars), TypeError);
         });
-        it('options.expression is undefined', function () {
-            assert.throws(() => { evaluate([], {}); }, TypeError)
+        it('options.trigger is undefined', async function () {
+            await expectThrow(() => evaluate(vars, {}), TypeError);
         });
-        it('options.expression is not a string', function () {
-            assert.throws(() => { evaluate([], {expression: null}); }, TypeError)
+        it('options.trigger is null', async function () {
+            await expectThrow(() => evaluate(vars, {trigger: null}), TypeError);
         });
-        it('options.trigger is undefined', function () {
-            assert.throws(() => { evaluate([], {expression: ''}); }, TypeError)
+        it('options.expression is undefined', async function () {
+            await expectThrow(() => evaluate(vars, {trigger: ''}), TypeError);
         });
-        it('options.trigger is null', function () {
-            assert.throws(() => { evaluate([], {expression: '', trigger: null}); }, TypeError)
+        it('options.expression is not a string', async function () {
+            await expectThrow(() => evaluate(vars, {trigger: '', expression: true}), TypeError);
         });
     });
 
     describe('Input is empty string', function () {
-        let result;
-        it('does not throw an error', function () {
-            result = evaluate([], {trigger: '', expression: ''});
+        it('does not throw an error', async function () {
+            await evaluate(vars, {trigger: '', expression: ''});
         });
-        it('returns a string', function () {
+        it('returns a string', async function () {
+            let result = await evaluate(vars, {trigger: '', expression: ''});
             assert.equal(typeof(result), 'string');
         });
-        it('string is empty', function () {
-            assert.equal(result, '');
+        it('string is empty', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: ''}), '');
         });
     });
 
-    describe('Input with no significant characters', function () {
-        let result;
-        it('does not throw an error', function () {
-            result = evaluate([], {trigger: '', expression: 'plain text'});
+    describe('Input is plain text', function () {
+        it('does not throw an error', async function () {
+            await evaluate(vars, {trigger: '', expression: 'plain text'});
         });
-        it('returns a string', function () {
+        it('returns a string', async function () {
+            let result = await evaluate(vars, {trigger: '', expression: 'plain text'});
             assert.equal(typeof(result), 'string');
         });
-        it('string matches input', function () {
-            assert.equal(result, 'plain text');
+        it('string matches input', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: 'plain text'}) , 'plain text');
         });
     });
 
-    describe('Input with escape sequences', function () {
-        it('Escapes \\\\', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '\\\\'}), '\\');
+    describe('Input is escape sequences', function () {
+        it('Escapes \\\\', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\\\'}), '\\');
         });
-        it('Escapes \\$', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '\\$'}), '$');
+        it('Escapes \\$', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\$'}), '$');
         });
-        it('Escapes \\"', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '\\"'}), '"');
+        it('Escapes \\"', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\"'}), '"');
         });
-        it('Treats \\ at the end of an expression as a literal', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '\\'}), '\\');
-        });
-        it('Handles leading text', function () {
-            assert.equal(evaluate([], {trigger: '', expression: 'text\\\\'}), 'text\\');
-        });
-        it('Handles trailing text', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '\\\\text'}), '\\text');
-        });
-        it('Treats non-escape-sequences as literal \\', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '\\a'}), '\\a');
+        it('Treats \\ at the end of an expression as a literal', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\'}), '\\');
         });
     });
 
-    describe('Input with quotes', function () {
-        it('Throws an error if there is no applicable closing quote', function () {
-            assert.throws(() => { evaluate([], {trigger: '', expression: '"'})}, errors.ExpressionSyntaxError);
+    describe('Input is quoted text', function () {
+        it('Throws an error if there is no applicable closing quote', async function () {
+            await expectThrow(() => evaluate(vars, {trigger: '', expression: '"'}), errors.ExpressionSyntaxError);
         });
-        it('Returns an empty string for ""', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '""'}), '');
+        it('Returns text inside of quotes without quation marks', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '"text"'}), 'text');
         });
-        it('Does not include surrounding quotes in result', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '"text"'}), 'text');
+        it('Returns an empty string for ""', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '""'}), '');
         });
-        it('Treats $ as plain text', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '"ltext $var ttext"'}), 'ltext $var ttext')
+        it('Treats $ as plain text', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '"$var"'}), '$var');
         });
-        it('Escapes \\\\', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '"\\\\"'}), '\\');
+        it('Escapes \\\\', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '"\\\\"'}), '\\');
         });
-        it('Escapes \\"', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '"\\""'}), '"');
+        it('Escapes \\"', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '"\\""'}), '"');
         });
-        it('Treats non-escape-sequence as literal \\', function () {
-            assert.equal(evaluate([], {trigger: '', expression: '"\\a"'}), '\\a');
+        it('Treats non-escape-sequence as literal \\', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '"\\a"'}), '\\a');
         });
-        describe('Can be mixed with:', function () {
-            it('Plain Text', function () {
-                assert.equal(evaluate([], {trigger: '', expression: 'leading"text"'}), 'leadingtext');
-                assert.equal(evaluate([], {trigger: '', expression: 'leading "text"'}), 'leading text');
-                assert.equal(evaluate([], {trigger: '', expression: '"text"trailing'}), 'texttrailing');
-                assert.equal(evaluate([], {trigger: '', expression: '"text" trailing'}), 'text trailing');
-                assert.equal(evaluate([], {trigger: '', expression: 'leading"text"trailing'}), 'leadingtexttrailing');
-                assert.equal(evaluate([], {trigger: '', expression: 'leading "text"trailing'}), 'leading texttrailing');
-                assert.equal(evaluate([], {trigger: '', expression: 'leading"text" trailing'}), 'leadingtext trailing');
-                assert.equal(evaluate([], {trigger: '', expression: 'leading "text" trailing'}), 'leading text trailing');
-            });
-            it('Escape Sequences', function () {
-                assert.equal(evaluate([], {trigger: '', expression: '\\$"text"'}), '$text');
-                assert.equal(evaluate([], {trigger: '', expression: '\\$ "text"'}), '$ text');
-                assert.equal(evaluate([], {trigger: '', expression: '"text"\\\\'}), 'text\\');
-                assert.equal(evaluate([], {trigger: '', expression: '"text" \\\\'}), 'text \\');
-                assert.equal(evaluate([], {trigger: '', expression: '\\$"text"\\\\'}), '$text\\');
-                assert.equal(evaluate([], {trigger: '', expression: '\\$ "text"\\\\'}), '$ text\\');
-                assert.equal(evaluate([], {trigger: '', expression: '\\$"text" \\\\'}), '$text \\');
-                assert.equal(evaluate([], {trigger: '', expression: '\\$ "text" \\\\'}), '$ text \\');
-            });
+    });
+
+    describe('Input is variable', function () {
+        it('Treats naked $ as text', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '$'}), '$');
+        });
+        it('Treats $<number> as text', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '$10'}), '$10');
+        });
+        it('Throws an error if the variable does not exist', async function () {
+            await expectThrow(() => evaluate(vars, {trigger: '', expression: '$notdefined'}), errors.ExpressionVariableError);
+        });
+        it('Evaluates variable', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '$txt'}), 'evaled_var_text');
+        });
+        it('Evaluates variable with arguments', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '$sum[1,2]'}), '3');
+        });
+        it('Evaluates nested variables', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '$sum[$ten, 1]'}), '11');
+        });
+        it('Evaluates nested variable with arguments', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '$sum[$sum[$ten, 1], 1]'}), '12');
+        });
+    });
+
+    describe('Input is a mix', async function () {
+        it('Plain-text and escape sequences', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: 'text\\\\'}), 'text\\');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\\\text'}), '\\text');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: 'text\\\\text'}), 'text\\text');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\\\text\\\\'}), '\\text\\');
+        });
+        it('Treats non-escape-sequences as literal \\', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\a'}), '\\a');
+        });
+        it('Plain-text, escape sequences and quotes', async function () {
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: 'leading"text"'}), 'leadingtext');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: 'leading "text"'}), 'leading text');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '"text"trailing'}), 'texttrailing');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '"text" trailing'}), 'text trailing');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: 'leading"text"trailing'}), 'leadingtexttrailing');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: 'leading "text"trailing'}), 'leading texttrailing');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: 'leading"text" trailing'}), 'leadingtext trailing');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: 'leading "text" trailing'}), 'leading text trailing');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\$"text"'}), '$text');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\$ "text"'}), '$ text');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '"text"\\\\'}), 'text\\');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '"text" \\\\'}), 'text \\');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\$"text"\\\\'}), '$text\\');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\$ "text"\\\\'}), '$ text\\');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\$"text" \\\\'}), '$text \\');
+            await expectEqual(() => evaluate(vars, {trigger: '', expression: '\\$ "text" \\\\'}), '$ text \\');
         });
     });
 });
