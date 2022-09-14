0fba69e780749b50bdccc7ad740572c531606c0d
SReject <SReject@users.noreply.github.com>
2022-09-14 08:51:17 -0400

    fix: textToken not setting type

--- src/tokens/text.ts
+++ src/tokens/text.ts
@@ -1,8 +1,12 @@
+import TokenType from '../types/token-types';
 import Token, { IToken } from './base';
 
 export default class TextToken extends Token {
     public value: string;
     constructor(token: IToken) {
-        super(token);
+        super({
+            ...token,
+            type: TokenType.TEXT
+        });
     }
 }
