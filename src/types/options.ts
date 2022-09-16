3eeaf731d986c19cfd40ffeb1c71e3afffd5a435
SReject <SReject@users.noreply.github.com>
2022-09-15 21:44:26 -0400

    feat: allow lookup handlers to be async

--- src/types/options.ts
+++ src/types/options.ts
@@ -3,7 +3,7 @@ interface Handler {
     evaluator: (meta: any, ...args: any[]) => any;
 }
 
-type LookupHandler = (name: string, meta?: any) => Handler;
+type LookupHandler = (name: string, meta?: any) => Handler | Promise<Handler>;
 
 export default interface ParserOptions {
     functionalHandlers: Record<string, LookupHandler>;
