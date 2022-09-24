f1bc127d8bcb677c1aaf03029c90b843945ff87b
SReject <SReject@users.noreply.github.com>
2022-09-24 04:57:18 -0400

    feat: make subject typed as any

--- src/helpers/has.ts
+++ src/helpers/has.ts
@@ -1,3 +1,4 @@
 const hasOwnProperty = Object.prototype.hasOwnProperty;
 
-export default (subject: unknown, key: string) => hasOwnProperty.call(subject, key);
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+export default (subject: any, key: string) => (hasOwnProperty.call(subject, key) && subject[key] !== undefined);
