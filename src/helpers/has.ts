e15fe267efd7bad26bea4141dbd7494cbfada56b
SReject <SReject@users.noreply.github.com>
2022-09-24 08:06:56 -0400

    fix: add null check

--- src/helpers/has.ts
+++ src/helpers/has.ts
@@ -1,4 +1,4 @@
 const hasOwnProperty = Object.prototype.hasOwnProperty;
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-export default (subject: any, key: string) => (hasOwnProperty.call(subject, key) && subject[key] !== undefined);
+export default (subject: any, key: string) => (subject != null && hasOwnProperty.call(subject, key) && subject[key] !== undefined);
