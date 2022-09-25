1e6d1759b65b3f1ac360d4233540a191bf6f8969
SReject <SReject@users.noreply.github.com>
2022-09-25 06:24:54 -0400

    chore(tests): fix transform regex

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     transform: {
-        '.ts$': [
+        '\\.ts$': [
             'ts-jest',
             {
                 tsconfig: "./tsconfig.json"
